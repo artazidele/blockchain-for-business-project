be95b1fea76895a8c79bb8d3309701fad340ef72
artazidele <77989249+artazidele@users.noreply.github.com>
2024-12-04 01:03:13 +0200

    fix: show last project

--- client/src/project/components/AllProjects.js
+++ client/src/project/components/AllProjects.js
@@ -8,10 +8,46 @@ const { ethers } = require("ethers");
 export function AllProjects() {
 
     const [projects, setProjects] = useState([]);
+    const [projects1, setProjects1] = useState([]);
+    const [count, setCount] = useState([]);
 
     useEffect(() => {
         getProjects();
     }, []);
+
+    useEffect(() => {
+        getLastProject();
+    }, [count]);
+
+    async function getLastProject() {
+        const contractAddress = localStorage.getItem('contractAddress');
+        const signerAddress = localStorage.getItem('signerAddress');
+        const id = projects.length;
+
+        try {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner(signerAddress);
+            const thisContract = new ethers.Contract(contractAddress, CharityPlatformContract.abi, signer);
+
+            const projectWithId = await thisContract.getProject(id);
+            // console.log(projectWithId);
+            const mc = projectWithId.milestoneCount.toString()
+            const project = {
+                id: id,
+                name: projectWithId.name,
+                charityAddress: projectWithId.charityAddress,
+                goalAmount: projectWithId.goalAmount,
+                raisedAmount: projectWithId.raisedAmount,
+                isActive: projectWithId.isActive,
+                milestoneCount: mc,
+            };
+            const allProjectsArray = projects;
+            allProjectsArray.push(project);
+            setProjects1(allProjectsArray);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
 
     async function getProjects() {
         const contractAddress = localStorage.getItem('contractAddress');
@@ -46,6 +82,8 @@ export function AllProjects() {
                 allProjectsArray.push(project);
             };
             setProjects(allProjectsArray);
+            setCount(names.length);
+
         } catch (error) {
             console.error("Error:", error);
         }
@@ -55,7 +93,8 @@ export function AllProjects() {
         <div>
             <h1 className='ml-6 my-8 text-4xl italic text-coffee_5 font-semibold'>All projects</h1>
             <div className='grid grid-cols-3 gap-4 w-full'>
-                {projects && projects.map((project) => <div key={project.id}>
+                {projects1 && projects1.map((project) => <div key={project.id}>
+                    {/* <p>{project.id}</p> */}
                     <ProjectsItem project={ project }/>
                 </div>)}
             </div>
