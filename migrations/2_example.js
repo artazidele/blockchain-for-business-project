3bb7394fe69447f8061a469fd005d9df2d95ee55
artazidele <77989249+artazidele@users.noreply.github.com>
2025-01-07 16:50:53 +0200

    fix: remove test files

--- migrations/2_example.js
+++
@@ -1,6 +0,0 @@
-const Example = artifacts.require("Example");
-
-module.exports = function(deployer) {
-    const initialSupply = web3.utils.toWei("1000000", "ether");
-  deployer.deploy(Example, initialSupply);
-};
